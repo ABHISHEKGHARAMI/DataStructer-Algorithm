46b13c6552f976a10ef27c1d8cc56ceca0d442d6
abhishek <abhishekgharami1998@gmail.com>
2025-05-26 18:46:25 +0530

    setting up the whole thing

---
+++ Introduction_of_c++/first1.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main() {
+    cout << "Hello, World!" << endl; // Output a greeting message
+    return 0; // Indicate that the program ended successfully
+}
