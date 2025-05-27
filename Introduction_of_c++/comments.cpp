4b0ea01a1ce9026cdd5c8ae45a0baf70f271e44e
abhishek <abhishekgharami1998@gmail.com>
2025-05-27 21:46:51 +0530

    reding about the comments

---
+++ Introduction_of_c++/comments.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+// This program demonstrates the use of comments in C++.
+int main(){
+    // This is a single-line comment explaining the next line of code.
+    cout << "hello world" << endl; // Output a greeting message
+    /*
+        * This is a multiline comments.
+    */
+   cout << "this is a comments" << endl; // Output a message about comments
+    // Comments are using to explain the code and make it more explanatory.
+    return 0;  // Indicate that the program ended successfully.
+}
