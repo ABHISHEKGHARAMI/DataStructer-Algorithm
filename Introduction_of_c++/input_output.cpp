661503b1a8ce13e4e5111b8a2e65d0a052bf05a0
abhishek <abhishekgharami1998@gmail.com>
2025-05-30 21:43:57 +0530

    adding input output for the program

---
+++ Introduction_of_c++/input_output.cpp
@@ -0,0 +1,15 @@
+// This is for the input output operation for the user
+#include<iostream>
+using namespace std;
+int main(){
+    // iostream stands for the input output stream
+    cout << "Hello world";
+    // but if you print the next line here
+    cout << "welcome to the c++ world !!";
+    // now checkout the newline for the user
+    cout << "this is the first line" << endl;
+    cout << "this is the second line" << endl;
+    cout << "here we use the third line" << endl;
+    cout << "this is the fourth line" << endl;
+    return 0;
+}
