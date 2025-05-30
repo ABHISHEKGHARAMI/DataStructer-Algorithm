f5524e254f9d169d6721043b2272aa85611b9c07
abhishek <abhishekgharami1998@gmail.com>
2025-05-30 23:29:39 +0530

    adding new line \n in the program

--- Introduction_of_c++/input_output.cpp
+++ Introduction_of_c++/input_output.cpp
@@ -11,5 +11,16 @@ int main(){
     cout << "this is the second line" << endl;
     cout << "here we use the third line" << endl;
     cout << "this is the fourth line" << endl;
+    // typically developer or sde dont use the endl because its causing the system to  slow down
+    // because the endl have 2 jobs specifically
+    // 1. do the new line
+    // 2. second job is the flush the buffer so when we use the  multiple endl will flush the buffer multiple times
+    // so we can use the \n instead of endl
+    cout << "this is the fifth line\n";
+    cout << "this is the sixth line\n";
+    // we can also use the \n in the string
+    cout << "this is the seventh line\n";
+    // we can also use the \n in the string
+    cout << "this is the eighth line\n";
     return 0;
 }
