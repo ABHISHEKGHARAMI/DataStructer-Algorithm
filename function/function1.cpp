5fd87f4a34d33ddb2e7b4a7e53a5cdc6a2351d58
abhishek <abhishekgharami1998@gmail.com>
2025-06-05 15:21:32 +0530

    adding the more type funcanality

--- function/function1.cpp
+++ function/function1.cpp
@@ -10,6 +10,7 @@
 #include<iostream>
 // using the libary function for the algorithms
 #include<algorithm>
+#include<cmath>
 using namespace std;
 // first there no return type of function
 void display(){
@@ -20,6 +21,24 @@ float simpleInterest(float p,float r,float t){
     float si;
     si = (p * r * t)/100;
     return si;
+}
+// default parameter for the  function
+double defaultParameter(int x,int y=3){
+  return pow(x,y);
+}
+// but this is for the Ologn for the pow function
+long long firstPow(int base,int exp){
+  long long result = 1;
+  while (exp > 0)
+  {
+    if (exp%2 == 1)
+    {
+      result*=base;
+    }
+    base *= base;
+    exp = exp / 2;
+  }
+  return result;
 }
 int main(){
     float p,r,t;
@@ -33,5 +52,11 @@ int main(){
     cout << "\nenter two integer numbers:\n";
     cin >> a >> b;
     cout << "\nthe max of the " << a << " and " << b <<" is :" << max(a,b);
+    // observe the default parameter for the function
+    cout << "Showing the default parameter:\t" << defaultParameter(b);
+    cout << "Showing the fastest power for the long integer:";
+    long long d;
+    d = firstPow(a,b);
+    cout << "The Fastest power of "<<"\t"<<a <<"\tof the\t"<<b <<"\tis\t"<<"is : "<<d;
     return 0;
 }
