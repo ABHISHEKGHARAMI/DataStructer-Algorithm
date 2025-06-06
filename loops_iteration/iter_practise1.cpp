adfe14eb9e627d4e648947d27ade4f414838c63d
abhishek <abhishekgharami1998@gmail.com>
2025-06-06 20:48:46 +0530

    adding the practise question for the loop

---
+++ loops_iteration/iter_practise1.cpp
@@ -0,0 +1,15 @@
+// here we practice question for iteration
+// print the number 1 - 100 using the loop 
+#include<iostream>
+#include<cmath>
+#include<algorithm>
+using namespace std;
+int main(){
+    cout << "printing the 1 -100 using the loop:";
+    for (int i = 1; i <=100; i++)
+    {
+        cout << "\n" << i;
+    }
+    
+    return 0;
+}
