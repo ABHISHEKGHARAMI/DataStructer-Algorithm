80d1aca6c5d13b9e3881bc6ed038305a57e2b620
abhishek <abhishekgharami1998@gmail.com>
2025-06-06 16:25:17 +0530

    adding the control at the nested loop

--- loops_iteration/nesting_iteration1.cpp
+++ loops_iteration/nesting_iteration1.cpp
@@ -13,6 +13,19 @@ int main(){
         }
         cout << "\n";
     }
+    // here we learn about the control of the loop or iteration
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            if (i==3 && j==3)
+            {
+                continue;
+            }
+            cout << '\t' <<'[' << i << ']' << '[' << j << ']';
+        }
+        cout <<"\n";
+    }
     
     return 0;
 }
