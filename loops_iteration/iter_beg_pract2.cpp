9f0e9b70313a5219fc8fdbbf7fb5bc0cf784489c
abhishek <abhishekgharami1998@gmail.com>
2025-06-06 20:58:04 +0530

    adding the 3rd question

---
+++ loops_iteration/iter_beg_pract2.cpp
@@ -0,0 +1,13 @@
+// print the multiplication table of 5
+#include<iostream>
+using namespace std;
+int main(){
+    cout << "print the multiplication table of the 5 :";
+    for (int i = 1; i <= 10; i++)
+    {
+        // creating the table
+        cout <<"\n" << 5 << " * " << i << " = " << 5*i;
+    }
+    
+    return 0;
+}
