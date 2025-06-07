8cd268ab1bb00659ee4ffe3f0dfebe491859b810
abhishek <abhishekgharami1998@gmail.com>
2025-06-07 12:46:43 +0530

    adding the string pallindrome also

--- loops_iteration/iter_inter_pract2.cpp
+++ loops_iteration/iter_inter_pract2.cpp
@@ -1,8 +1,10 @@
 // Question : Check a number is pallindrome or not
+// Question added : Check a string is pallindrome or not
 // Approach : first count the digit of the number the loop from the both side
-//  if unmatch raise the flag then declare it is not the pallindrome
+//  if unmatch raise the flag then declare it is not the pallindrome number
 #include<iostream>
 #include<cmath>
+#include<string>
 #include<algorithm>
 using namespace std;
 int countDigit(int number){
@@ -37,18 +39,76 @@ bool checkPallindrome(int number){
     }
     
 }
+// function to check the string is pallindrome or not
+bool checkStringPallindrome(string str1){
+    int i = 0;
+    int j = str1.length() - 1;
+    int flag = 0;
+    while (i <= j)
+    {
+        if (str1[i] != str1[j])
+        {
+            flag = 1;
+            break;
+        }
+        i++;
+        j--;
+    }
+    if (flag == 1)
+    {
+        return false;
+    }else{
+        return true;
+    }
+    
+    
+}
 int main(){
     int number;
-    cout << "enter a number to check it it is pallindrome :" ;
-    cin >> number ;
-    bool des = checkPallindrome(number) ;
-    if (des == true)
+    bool desc;
+    int options;
+    string str1;
+    do{
+    //  menu options
+    cout << "options for pallindrome" ;
+    cout << "\n 1: check for number.";
+    cout << "\n 2: check for string." ;
+    cout << "\n 0: for exit." ; 
+    cout << "\n enter the option:" ;
+    cin >> options;
+    switch (options)
     {
-        cout << "\n" << number << " is a pallindrome number" ;
+    case 1:
+        cout << "enter a number to check it it is pallindrome :";
+        cin >> number;
+        if (checkPallindrome(number) == true)
+        {
+            cout << number << "\t is pallindrome number";
+        }else{
+            cout << number << "\t is not a pallindrome number";
+        }
+        
+        break;
+    case 2:
+        cout << "enter the string to check it is pallindrome or not :";
+        cin >> str1;
+        if (checkStringPallindrome(str1) == true)
+        {
+            cout << str1 << "\t is a pallindrome";
+        }else{
+            cout << str1 << "\t is not a pallindrome";
+        }
+        break;
+    
+    case 0:
+       cout << "exiting the program , good-bye....";
+       break;
+        
+    
+    default:
+        cout << "wrong options...";
     }
-    else{
-        cout << "\n" << number << "is not a pallindrome number";
-    }
+    }while(options !=0);
     
     return 0;
 }
