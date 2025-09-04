d863ce6496fd0b639b64a3771a6ee6250916d729
abhishek <abhishekgharami1998@gmail.com>
2025-09-04 22:24:07 +0530

    testing with the string class .

---
+++ strings/stringClass2.cpp
@@ -0,0 +1,34 @@
+// here we practise all the things about the string class
+#include<iostream>
+#include<string>
+#include<stdexcept>
+#include"../log/log_program.hpp"
+int main(int argc,char* agv[]){
+    LOG_INFO("Program started...");
+    std::string s1;
+    // first take the input as string
+    std::cout << "enter the first string :" << std::endl;
+    std::getline(std::cin,s1);
+    // second the copy the specific string
+    int size = s1.size();
+    std::cout << "the size of the string is : " << size << std::endl;
+    LOG_INFO("The size of the string is : " + std::to_string(size));
+    int len;
+    std::cout << "enter to length of the string to copy to another string : " << std::endl;
+    std::cin >> len;
+    if (len < size)
+    {
+        // copy the specific length of the string
+        std::string s2(s1,len,size);
+        std::cout << "the string after copying is : " << s2 << std::endl;
+        LOG_INFO("after coping the string is " + s2);
+        std::cout << "the size of the string is : " << s2.size() << std::endl;
+        LOG_INFO("The size of the string is : " + std::to_string(s2.size()));
+        std::cout << "The capacity of the string is : " << s2.capacity() << std::endl;
+        LOG_INFO("the capacity of the string is : " + std::to_string(s2.capacity()));
+        
+    }
+    
+    LOG_INFO("Program ended..");
+    return 0;
+}
