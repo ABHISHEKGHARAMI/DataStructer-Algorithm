75a9ab02ca625c5a28ea0de13fb0a8b4162521fb
abhishek <abhishekgharami1998@gmail.com>
2025-09-14 21:39:32 +0530

    fixing the string with different string method..

--- strings/basicStringClass1.cpp
+++ strings/basicStringClass1.cpp
@@ -6,7 +6,24 @@
 int main(int argc,char* argv[]){
     std::string s1;
     std::cout << "enter a string :" << std::endl;
-    std::cin >> s1;
+//    user input for the line
+    std::getline(std::cin,s1);
     std::cout << "empty : " << (s1.empty() ? "yes" : "no") << std::endl;
+    // ok now we have now understand to point of the empty method
+    // now we are going to use th at or the [] operator
+    int pos;
+    std::cout << "enter a number want to print the postion of the string : ";
+    std::cin >> pos;
+    if (pos < 0 || pos > (int)s1.size())
+    {
+        // invalid postion of the string
+        std::cout << "the position user entered is invalid..." << std::endl;
+        LOG_ERROR("user entered the invalid input...");
+    }
+    else{
+        std::cout << "the char at the pos is : " << s1.at(pos) << std::endl;
+        LOG_INFO("The char at the postion is : " + std::string(1,s1[pos]));
+    }
+    
     return 0;
 }
